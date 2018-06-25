daaef9b688d004fee38b84f276e90ea1741c9346
alidep <amdepsky@gmail.com>
2018-06-25 00:46:08 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,16 +1,24 @@
-function setup() {
-   createCanvas(windowWidth, windowHeight);
-}
 
+var barWidth = 20;
+var lastBar = -1;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  colorMode(HSB, height, height, height);  
+  noStroke();
+  background(0);
+}
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
+  var whichBar = mouseX / barWidth;
+  if (whichBar !== lastBar) {
+    var barX = whichBar * barWidth;
+    fill(mouseY, height, height);
+    rect(barX, 0, barWidth, height);
+    lastBar = whichBar;
   }
-  ellipse(mouseX, mouseY, 80, 80);
 }
+
 
 function windowResized() {
    resizeCanvas(windowWidth, windowHeight);
